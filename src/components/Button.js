5cf527c7c7e161a8745e54e0b13fa0170ecfdb5c
KozubIgn <ignacykozub@gmail.com>
2021-04-21 19:19:17 +0200

    fix spaces

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,12 +4,12 @@ const Button = ({color, text, onClick}) => {
 
     return (
         <button onClick={onClick} style={{backgroundColor: color}}
-            className='btn'>{text}
+                className='btn'>{text}
         </button>
     );
 }
 Button.defaultProps = {
-    color:'steelblue',
+    color: 'steelblue',
 }
 
 Button.propTypes = {
