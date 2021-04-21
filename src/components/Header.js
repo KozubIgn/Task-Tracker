d0cfc11070b0138f19b4f0d7a94baa7411baa5eb
KozubIgn <ignacykozub@gmail.com>
2021-04-21 19:18:51 +0200

    hide add button using useLocation hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types'
 import Button from './Button';
+import {useLocation} from 'react-router-dom';
 //destructure in curly brackets - always pass object
 // dont need to write obj.title
 const Header = ({title, onAdd, showAdd}) => {
-
+const  location = useLocation()
     return (
         <header className='header'>
             <h1>{title}</h1>
+            {/*show when path location '/' is*/}
+            {location.pathname ==='/' &&
             <Button color={showAdd ? 'red' :'green'}
                     text={showAdd ? 'Close' : 'Add'}
-                    onClick={onAdd}/>
+                    onClick={onAdd}/>}
         </header>
     )
 
